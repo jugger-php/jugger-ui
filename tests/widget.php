3519897f694da53ecba3b5a7f2a8f594207bed9e
irpsv <i.rpsv@live.com>
2017-01-07 23:49:35 +0500

    refact

--- tests/widget.php
+++ tests/widget.php
@@ -5,18 +5,9 @@ use jugger\ui\Widget;
 
 class RetWidget extends Widget
 {
-    public $param1 = 'default';
-    public $param2;
-    public $param3;
-
     public function run()
     {
-        $params = [
-            "param1" => $this->param1,
-            "param2" => $this->param2,
-            "param3" => $this->param3,
-        ];
-        return json_encode($params);
+        return json_encode($this->params);
     }
 }
 
@@ -34,20 +25,20 @@ class WidgetTest extends TestCase
     {
         $this->assertEquals(
             RetWidget::widget(),
-            '{"param1":"default","param2":null,"param3":null}'
+            '[]'
         );
         $this->assertEquals(
             RetWidget::widget([
                 'param1' => 'value1'
             ]),
-            '{"param1":"value1","param2":null,"param3":null}'
+            '{"param1":"value1"}'
         );
         $this->assertEquals(
             RetWidget::widget([
                 'param2' => 'value2',
                 'param3' => 'value3',
             ]),
-            '{"param1":"default","param2":"value2","param3":"value3"}'
+            '{"param2":"value2","param3":"value3"}'
         );
         $this->assertEquals(
             RetWidget::widget([
