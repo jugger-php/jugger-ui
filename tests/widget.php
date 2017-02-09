2f06de91fb036efeec1eebbe56e732057cdad691
irpsv <i.rpsv@live.com>
2017-02-09 23:22:08 +0500

    add widget test

--- tests/widget.php
+++ tests/widget.php
@@ -3,58 +3,78 @@
 use PHPUnit\Framework\TestCase;
 use jugger\ui\Widget;
 
-class RetWidget extends Widget
+class Button extends Widget
 {
-    public function run()
-    {
-        return json_encode($this->params);
-    }
-}
+    public $id;
+    public $name;
+    public $value;
 
-class ViewWidget extends Widget
-{
+    public function init()
+    {
+        if (!$this->id && $this->name) {
+            $this->id = "{$this->name}-id";
+        }
+    }
+
     public function run()
     {
-        echo $this->getViewsPath();
+        return "<button name='{$this->name}' id='{$this->id}'>{$this->value}</button>";
     }
 }
 
 class WidgetTest extends TestCase
 {
-    public function testRetWidget()
+    public function testBase()
     {
+        $btn = new Button();
         $this->assertEquals(
-            RetWidget::widget(),
-            '[]'
+            "<button name='' id=''></button>",
+            $btn->render()
         );
+
+        $btn = new Button([
+            'name' => 'test',
+        ]);
         $this->assertEquals(
-            RetWidget::widget([
-                'param1' => 'value1'
-            ]),
-            '{"param1":"value1"}'
+            "<button name='test' id='test-id'></button>",
+            $btn->render()
         );
+
+        $btn = new Button([
+            'name' => 'test',
+            'value' => 'test value',
+        ]);
         $this->assertEquals(
-            RetWidget::widget([
-                'param2' => 'value2',
-                'param3' => 'value3',
-            ]),
-            '{"param2":"value2","param3":"value3"}'
+            "<button name='test' id='test-id'>test value</button>",
+            $btn->render()
         );
+
+        $btn = new Button([
+            'id' => 'id-test',
+            'name' => 'test',
+            'value' => 'test value',
+        ]);
         $this->assertEquals(
-            RetWidget::widget([
-                'param1' => 'value1',
-                'param2' => 'value2',
-                'param3' => 'value3',
-            ]),
-            '{"param1":"value1","param2":"value2","param3":"value3"}'
+            "<button name='test' id='id-test'>test value</button>",
+            $btn->render()
         );
     }
 
-    public function testViewWidget()
+    public function testConstruct()
     {
+        $btn1 = new Button([
+            'id' => '123',
+            'name' => '456',
+            'value' => '789',
+        ]);
+        $btn2 = new Button();
+        $btn2->id = '123';
+        $btn2->name = '456';
+        $btn2->value = '789';
+
         $this->assertEquals(
-            ViewWidget::widget(),
-            __DIR__.'/views'
+            $btn1->render(),
+            $btn2->render()
         );
     }
 }
